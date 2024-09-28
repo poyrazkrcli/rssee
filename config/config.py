4598424fbec8927935cd9cc5c69abea3c447b00d
poyrazkrcli <poyrazkrcli@gmail.com>
2024-09-28 21:12:04 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -41,7 +41,7 @@ BOT_ID = getenv("BOT_ID")
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/poyrazkrcli/rssee")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TheTeamAlexa/AlexaMusic")
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
